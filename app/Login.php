3aedc72ec9ee88dfdef2fc5f35cd9bbad2693549
dioh0922 <43507423+dioh0922@users.noreply.github.com>
2019-09-06 22:11:03 +0900

    ログイン処理の不備の修正
    ・クエリが発行できていない点を修正
    ・パスワードをハッシュとの比較で判定するように変更

--- app/Login.php
+++ app/Login.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Login extends Model
 {
@@ -11,13 +12,14 @@ class Login extends Model
     protected $table = "login"; //接続するテーブルの指定
 
     public function auth_check($ID, $pass){
-      $get_columm = Login::select("accept")
-                      ->where("userId", $ID)
-                      ->where("pass", $pass)
-                      ->first();
+			$get_columm = DB::table("login")
+											->select("pass", "accept")
+											->where("userID", $ID)
+											->first();
 
-      if( (!empty($get_columm->accept))
-      &&  ($get_columm->accept == 1) ){
+			if( (password_verify($pass, $get_columm->pass))
+			&& ( ($get_columm->accept == 1) ) )
+			{
         return true;
       }else{
         return false;
